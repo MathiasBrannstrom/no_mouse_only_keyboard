94314cbf2c704ba0553ee5eb8ad446b057dea5d8
MathiasBrannstrom <mathias.brannstrom@gmail.com>
2024-03-17 21:59:07 +0100

    Refactor a bit, to store actions rather than directions. Although not sure this is actually better as I realized I anyway need to deal with mouse clicks independently.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::{collections::HashMap, hash::Hash, process, sync::Arc, thread::sleep, time::Duration};
+use std::{borrow::Borrow, collections::HashMap, hash::Hash, process, sync::Arc, thread::sleep, time::Duration};
 
-use inputbot::{KeybdKey::{self, *}, MouseCursor};
+use inputbot::{KeybdKey::{self, *}, MouseButton, MouseCursor};
 
 #[derive(Eq, PartialEq, Hash)]
-enum MouseMove {
-    Up,
-    Down,
-    Left,
-    Right
+enum Action {
+    MouseMoveUp,
+    MouseMoveDown,
+    MouseMoveLeft,
+    MouseMoveRight,
+
+    // MouseLeftClick,
 }
 
 fn main() {
@@ -16,18 +18,18 @@ fn main() {
 
     let mut keybinds = HashMap::new();
 
-    keybinds.insert(F13Key, MouseMove::Up);
-    keybinds.insert(F14Key, MouseMove::Left);
-    keybinds.insert(F15Key, MouseMove::Down);
-    keybinds.insert(F23Key, MouseMove::Right);
+    keybinds.insert(F13Key, Action::MouseMoveUp);
+    keybinds.insert(F14Key, Action::MouseMoveLeft);
+    keybinds.insert(F15Key, Action::MouseMoveDown);
+    keybinds.insert(F23Key, Action::MouseMoveRight);
 
     start_loop(&keybinds, speed_arc)
 
 }
 
-fn start_loop(keybinds: &HashMap<KeybdKey, MouseMove>, speed_arc: Arc<i32>) {
+fn start_loop(keybinds: &HashMap<KeybdKey, Action>, speed_arc: Arc<i32>) {
 
-    let directions = create_directions_hashmap();
+    let actions = create_actions_hashmap(speed_arc);
     loop {
         
         if F10Key.is_pressed() {
@@ -35,8 +37,10 @@ fn start_loop(keybinds: &HashMap<KeybdKey, MouseMove>, speed_arc: Arc<i32>) {
         }
         for (key, mouse_move) in keybinds {
             if key.is_pressed() {
-                let (x,y) = directions.get(mouse_move).unwrap();
-                MouseCursor::move_rel(*x * *speed_arc, *y * *speed_arc);
+                if let Some(action) = actions.get(mouse_move) {
+                    action();
+                }
+                
             }
         }
 
@@ -44,35 +48,30 @@ fn start_loop(keybinds: &HashMap<KeybdKey, MouseMove>, speed_arc: Arc<i32>) {
     }
 }
 
-fn create_directions_hashmap() -> HashMap<MouseMove, (i32, i32)> {
-    let mut directions = HashMap::new();
+fn create_actions_hashmap(speed_arc: Arc<i32>) -> HashMap<Action, Box<dyn Fn()>> {
+    let mut actions: HashMap<Action, Box<dyn Fn()>> = HashMap::new();
 
-    directions.insert(MouseMove::Up, (0, -1));
-    directions.insert(MouseMove::Left, (-1, 0));
-    directions.insert(MouseMove::Down, (0, 1));
-    directions.insert(MouseMove::Right, (1, 0));
-    directions
+    let speed_clone = speed_arc.clone();
+    actions.insert(Action::MouseMoveUp, Box::new(move || MouseCursor::move_rel(0, -*speed_clone)));
+    
+    let speed_clone = speed_arc.clone();
+     actions.insert(Action::MouseMoveLeft, Box::new(move || MouseCursor::move_rel( -*speed_clone, 0)));
+    
+     let speed_clone = speed_arc.clone();
+    actions.insert(Action::MouseMoveDown, Box::new(move || MouseCursor::move_rel(0, *speed_clone)));
+    
+    let speed_clone = speed_arc.clone();
+    actions.insert(Action::MouseMoveRight, Box::new(move || MouseCursor::move_rel(*speed_clone, 0)));
+
+    // actions.insert(Action::MouseLeftClick, Box::new(|| {
+    //     MouseButton::LeftButton.press();
+    //     MouseButton::LeftButton.release();
+    // }
+    // ));
+    
+    actions
 }
 
-// fn set_up_key_for_direction(key: KeybdKey, dx: i32, dy: i32, speed: Arc<i32>) {
-    
-//     let is_pressed = Arc::new(Mutex::new(false));
-
-//     key.bind(move || {
-//         let mut is_pressed = is_pressed.lock().unwrap();
-
-//         if *is_pressed {
-//             return;
-//         }
-
-//         while key.is_pressed() {
-//             *is_pressed = true;
-//             MouseCursor::move_rel(dx* *speed, dy* *speed);
-//             sleep(Duration::from_millis(10));
-//         }
-//         *is_pressed = false;
-//     });
-
+// fn setup_action(actions_map: &mut HashMap<Action, Box<dyn Fn()>>, action:Action, closure: Box<dyn Fn()>, speed_arc: Arc<i32>) {
+//     actions_map.insert(action, v)
 // }
-
-
